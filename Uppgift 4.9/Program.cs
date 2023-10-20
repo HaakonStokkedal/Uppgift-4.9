aa9d6943e6f58cfe8398857579809e4ec5b9cea3
hast06001 <hast06001@E-PW024NW1>
2023-10-20 10:49:06 +0200

    Add project files.

---
+++ Uppgift 4.9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Uppgift_4._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur lång ska sidlängden i triangeln vara?");
+            int sidlängd = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < sidlängd; i++)
+            {
+                for(int j = i; j+1 > 0; j-- )
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
